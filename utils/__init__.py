cf6645cab7f9522047cf90a9de470c0d4c0c3b4b
Kirill Elagin <kirelagin@gmail.com>
2015-03-09 13:35:11 +0300

    Reorganise JSON fields fixers

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,30 @@
+from datetime import datetime, timedelta
 import json
 
 
-def load_json_fields(data):
+def recursive_fix(data, fix):
+    if not data:
+        return
     for k, v in data.items():
+        if v:
+            data[k] = fix(k, v)
+        if k.endswith('_json'):
+            recursive_fix(data[k], fix)
+
+def load_json_fields(data):
+    def fix(k, v):
         if k.endswith('_json') and v:
-            data[k] = json.loads(v)
-            load_json_fields(data[k])
+            return json.loads(v)
+        return v
+    recursive_fix(data, fix)
+
+def parse_times(data):
+    def fix(k, v):
+        if k in ('ctime', 'etime', 'last_check', 'last_success',):
+            d = datetime.fromtimestamp(int(v))
+            return '{} ({})'.format(d.isoformat(), v)
+        elif k in ('expire_in',):
+            d = timedelta(seconds=int(v))
+            return '{} ({})'.format(d, v)
+        return v
+    recursive_fix(data, fix)
